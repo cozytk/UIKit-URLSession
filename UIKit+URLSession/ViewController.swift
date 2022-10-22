2227db012d1ae628bcc3ad977c0886eb7d39dbee
Taekyun Kim <taekkim@student.42seoul.kr>
2022-10-22 16:19:15 +0900

    RxSwift + RxCocoa

--- UIKit+URLSession/ViewController.swift
+++ UIKit+URLSession/ViewController.swift
@@ -27,50 +27,25 @@ class ViewController: UIViewController {
             .disposed(by: disposeBag)
     }
 
-    private func getNotionAPI() -> Observable<String?> {
-        return Observable.create { emitter in
-            let headers = [
-                "accept": "application/json",
-                "Notion-Version": "2022-06-28",
-                "content-type": "application/json",
-                "authorization": "Bearer secret_lJJ6o18yolookxqAqlgKwUC3VFQMYlEige3enTaPIK7"
-            ]
+    private func getNotionAPI() -> Observable<String> {
+        let headers = [
+            "accept": "application/json",
+            "Notion-Version": "2022-06-28",
+            "content-type": "application/json",
+            "authorization": "Bearer secret_lJJ6o18yolookxqAqlgKwUC3VFQMYlEige3enTaPIK7"
+        ]
 
-            let parameters = ["page_size": 100] as [String : Any]
-            let postData = try! JSONSerialization.data(withJSONObject: parameters, options: [])
-            var request = URLRequest(url: URL(string: "https://api.notion.com/v1/databases/a255741aa68f43c89982561c09a1e3fe/query")!, cachePolicy: .useProtocolCachePolicy,
-                                     timeoutInterval: 10.0)
-            request.httpMethod = "POST"
-            request.allHTTPHeaderFields = headers
-            request.httpBody = postData as Data
+        let parameters = ["page_size": 100] as [String : Any]
+        let postData = try! JSONSerialization.data(withJSONObject: parameters, options: [])
+        var request = URLRequest(url: URL(string: "https://api.notion.com/v1/databases/a255741aa68f43c89982561c09a1e3fe/query")!, cachePolicy: .useProtocolCachePolicy,
+                                 timeoutInterval: 10.0)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        request.httpBody = postData as Data
+        let session = URLSession.shared
 
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error {
-                    emitter.onError(error)
-                    print(error.localizedDescription)
-                    return
-                }
-                guard let httpResponse = response as? HTTPURLResponse,
-                      (200...299).contains(httpResponse.statusCode) else {
-                    var response = response as? HTTPURLResponse
-                    print("Response Problem")
-                    return
-                }
-                guard let mimeType = httpResponse.mimeType, mimeType == "application/json", let data else {
-                    print("mimeType Problem")
-                    return
-                }
-                guard let sample = try? JSONDecoder().decode(NotionSample.self, from: data) else {
-                    print("sample Problem")
-                    return
-                }
-                emitter.onNext(sample.results.description)
-                emitter.onCompleted()
-            }
-            task.resume()
-            return Disposables.create()
-//                task.cancel()
-//            }
-        }
+        return session.rx.data(request: request)
+            .map { try JSONDecoder().decode(NotionSample.self, from: $0)}
+            .map { $0.results.description }
     }
 }
